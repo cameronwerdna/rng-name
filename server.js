35bb4410b6f10d9c7047f21231a337f12e8dd185
cam <cameron.a.sax@gmail.com>
2022-06-16 16:43:23 -0700

    more commits

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const data = require('./random-names.json')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.get('/random-name', (req, res) => {
     const { first_name } = data[Math.round(Math.random() * data.length)]
@@ -18,4 +20,4 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(3000, () => console.log("listening"))
+app.listen(process.env.PORT || 3000, () => console.log("listening"))
